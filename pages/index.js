35352b94f7f020d042125828ca9c2ecc2e992caa
Kyle Peeler <kylepeeler@gmail.com>
2021-03-11 08:53:18 -0500

    add spacing between style blocks

--- pages/index.js
+++ pages/index.js
@@ -44,6 +44,7 @@ const Home = () => {
           justify-content: center;
           align-items: center;
         }
+
         main {
           padding: 5rem 0;
           flex: 1;
@@ -52,6 +53,7 @@ const Home = () => {
           justify-content: center;
           align-items: center;
         }
+
         footer {
           width: 100%;
           height: 100px;
@@ -60,40 +62,49 @@ const Home = () => {
           justify-content: center;
           align-items: center;
         }
+
         footer img {
           margin-left: 0.5rem;
         }
+
         footer a {
           display: flex;
           justify-content: center;
           align-items: center;
         }
+
         a {
           color: inherit;
           text-decoration: none;
         }
+
         .title a {
           color: #0070f3;
           text-decoration: none;
         }
+
         .title a:hover,
         .title a:focus,
         .title a:active {
           text-decoration: underline;
         }
+
         .title {
           margin: 0;
           line-height: 1.15;
           font-size: 4rem;
         }
+
         .title,
         .description {
           text-align: center;
         }
+
         .description {
           line-height: 1.5;
           font-size: 1.5rem;
         }
+
         code {
           background: #fafafa;
           border-radius: 5px;
@@ -102,6 +113,7 @@ const Home = () => {
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
         }
+
         .grid {
           display: flex;
           align-items: center;
@@ -110,6 +122,7 @@ const Home = () => {
           max-width: 800px;
           margin-top: 3rem;
         }
+
         .card {
           margin: 1rem;
           flex-basis: 45%;
@@ -121,21 +134,25 @@ const Home = () => {
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
         }
+
         .card:hover,
         .card:focus,
         .card:active {
           color: #0070f3;
           border-color: #0070f3;
         }
+
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
+
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
+
         @media (max-width: 600px) {
           .grid {
             width: 100%;
