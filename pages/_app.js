90e059d4455ab56a71ca92a6eee6b3ec19f749a0
Kyle Peeler <kylepeeler@gmail.com>
2021-03-11 14:43:23 -0500

    add chakra ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,41 @@
-import '../styles/globals.css';
-import { AuthProvider } from '../lib/auth';
+import '@/styles/globals.css';
+import { ChakraProvider, CSSReset } from '@chakra-ui/react';
+import { Global, css } from '@emotion/react';
+
+import { AuthProvider } from '@/lib/auth';
+import customTheme from '@/styles/theme.js';
+
+const GlobalStyle = ({ children }) => {
+  return (
+    <>
+      <CSSReset />
+      <Global
+        styles={css`
+          html {
+            min-width: 360px;
+            scroll-behavior: smooth;
+          }
+
+          #__next {
+            display: flex;
+            flex-direction: column;
+            min-height: 100vh;
+          }
+        `}
+      />
+      {children}
+    </>
+  );
+};
 
 const App = ({ Component, pageProps }) => {
   return (
-    <AuthProvider>
-      <Component {...pageProps} />
-    </AuthProvider>
+    <ChakraProvider theme={customTheme}>
+      <AuthProvider>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </ChakraProvider>
   );
 };
 
